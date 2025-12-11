556ff581b716e1b2806431d784a715e54ff39ce5
ellina <157985958+ellinar5@users.noreply.github.com>
2025-12-11 11:14:44 +0700

    Add files via upload

---
+++ 5 ПАК - 5_OpenCV/5 семинар_ воиспреизведение видео в формате ЧБ.py
@@ -0,0 +1,21 @@
+import cv2
+import numpy as np
+
+path = r'E:/УЧЕБА/УЧЕБА НГУ/2 КУРС/1 СЕМЕСТР/ПИТОН/СЕМИНАРЫ/Задание_5 семинар/красиво_1.mp4'
+cap = cv2.VideoCapture(path) #Захват видео из файла
+
+#Чтение каждого кадра из видео
+while True:
+    ret, frame = cap.read() #прочитан ли кадр
+    if not ret:
+        break
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)  # перевод в серый
+    cv2.imshow('Gray Video', gray) #отбражение изображения в окошке
+
+    if cv2.waitKey(28) & 0xFF == 27: #выход по кнопки esc
+        break
+
+#освободить ресурсов
+cap.release()
+cv2.destroyAllWindows()
